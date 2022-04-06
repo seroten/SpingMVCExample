4cc1303c4c3858e58f86978eab5850c9e8bd3b78
Dmitry <seroten@mail.ru>
2022-04-06 18:49:48 +0300

    Second commit

--- src/main/java/org/example/config/MySpringMVCDispatcherServletInitializer.java
+++ src/main/java/org/example/config/MySpringMVCDispatcherServletInitializer.java
@@ -7,16 +7,16 @@ public class MySpringMVCDispatcherServletInitializer extends
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return null;
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[] {SpringConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[] {"/"};
     }
 }
--- src/main/java/org/example/config/SpringConfig.java
+++ src/main/java/org/example/config/SpringConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
@@ -14,7 +15,7 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 @Configuration
 @ComponentScan("org.example")
 @EnableWebMvc
-public class SpringConfig {
+public class SpringConfig implements WebMvcConfigurer {
     private final ApplicationContext applicationContext;
 
     @Autowired
