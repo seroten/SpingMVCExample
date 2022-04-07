7232fb5c3e149975efc32be20614fe5a2c995edc
Dmitry <seroten@mail.ru>
2022-04-07 17:13:05 +0300

    add model

--- src/main/java/org/example/controllers/FirstController.java
+++ src/main/java/org/example/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package org.example.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,8 +13,10 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
     @GetMapping("hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println(name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("surname", surname);
         return "first/hello";
     }
     @GetMapping("goodbye")
