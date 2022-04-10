fbea1b0f424f7b5bba2a1ec87f0e16aabb1bb27d
Dmitry <seroten@mail.ru>
2022-04-10 21:46:28 +0300

    added update and delete

--- src/main/java/org/example/DAO/PersonDAO.java
+++ src/main/java/org/example/DAO/PersonDAO.java
@@ -19,15 +19,25 @@ public class PersonDAO {
     }
 
     public List<Person> index() {
+        people.forEach(System.out::println);
         return people;
     }
 
     public Person show(int id) {
-        return people.stream().filter(people -> people.getId() == id).findAny().orElse(null);
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
     }
 
     public void save(Person person) {
         person.setId(++count);
         people.add(person);
+    }
+
+    public void update(int id, Person person) {
+        Person personToBeUpdated = show(id);
+        personToBeUpdated.setName(person.getName());
+    }
+
+    public void delete(int id) {
+        people.remove(id);
     }
 }
